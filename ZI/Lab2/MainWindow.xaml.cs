051eeea321d8bcb5eff51179ec26c27f8bb7d2a5
Андрей Чураков <andrey.the.knight@gmail.com>
2018-09-30 21:16:41 +0400

    1

---
+++ ZI/Lab2/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private class Data : INotifyPropertyChanged
+        {
+            private string plaintext;
+            private string ciphertext;
+            private string key;
+
+            public string Plaintext
+            {
+                get { return plaintext; }
+                set
+                {
+                    plaintext = value;
+                    OnPropertyChanged("Plaintext");
+                }
+            }
+            public string Ciphertext
+            {
+                get { return ciphertext; }
+                set
+                {
+                    ciphertext = value;
+                    OnPropertyChanged("Ciphertext");
+                }
+            }
+            public string Key
+            {
+                get { return key; }
+                set
+                {
+                    key = value;
+                    OnPropertyChanged("Key");
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged(string property)
+            {
+                try
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+                catch (NullReferenceException) { };
+            }
+
+            
+        }
+
+        private Data data = new Data();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = data;
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            data.Ciphertext = Desx.Encrypt(data.Plaintext);
+        }
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            data.Plaintext = Desx.Decrypt(data.Ciphertext);
+        }
+
+    }
+}
